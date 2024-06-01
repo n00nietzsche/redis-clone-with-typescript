36334707dd28516c3782eb8e6aaeba6db0069d84
jakeseo <n00nietzsche@gmail.com>
2024-06-02 03:48:46 +0900

    feat: 서버 생성 시 저장소를 주입받는 형태로 설정

--- app/main.ts
+++ app/main.ts
@@ -8,32 +8,38 @@ console.log(
   "Logs from your program will appear here!"
 );
 
-const store = new Store();
+export function getRedisServer(store: Store) {
+  const server = net.createServer(
+    (connection: net.Socket) => {
+      const commandExecutor = new CommandExecutor(
+        connection,
+        store
+      );
+
+      // Handle connection
+      connection.on("data", (data: Buffer) => {
+        console.log(
+          "Recieved: ",
+          JSON.stringify(data.toString())
+        );
+
+        const { command, args } =
+          parseClientCommand(data.toString());
+
+        commandExecutor.executeCommand(
+          command,
+          args
+        );
+      });
+    }
+  );
+
+  return server;
+}
 
 // Uncomment this block to pass the first stage
-const server: net.Server = net.createServer(
-  (connection: net.Socket) => {
-    const commandExecutor = new CommandExecutor(
-      connection,
-      store
-    );
-
-    // Handle connection
-    connection.on("data", (data: Buffer) => {
-      console.log(
-        "Recieved: ",
-        JSON.stringify(data.toString())
-      );
-
-      const { command, args } =
-        parseClientCommand(data.toString());
-
-      commandExecutor.executeCommand(
-        command,
-        args
-      );
-    });
-  }
+const server: net.Server = getRedisServer(
+  new Store()
 );
 
 server.listen(6379, "127.0.0.1");
