2990013d860f85f6f190e2ce60712e3eb143243b
jakeseo <n00nietzsche@gmail.com>
2024-06-06 11:25:15 +0900

    refactor: main.ts 파일에 대한 테스트 리팩토링
    - 서버를 매 명령어마다 새로 생성하는 것이 아니라 생성된 서버를 이용하도록 설정
    - sendInput 함수에서 사용하지 않는 server 파라미터 제거

--- test/main.test.ts
+++ test/main.test.ts
@@ -14,10 +14,7 @@ import {
 import { getRedisServer } from "../app/main";
 import { Store } from "../app/store";
 
-async function sendInput(
-  input: string,
-  server: net.Server
-) {
+async function sendInput(input: string) {
   const client = net.createConnection(
     { port: 6380, host: "127.0.0.1" },
     () => {
@@ -31,22 +28,21 @@ async function sendInput(
 }
 
 describe("Redis 서버 애플리케이션은", () => {
+  let server;
+
+  beforeEach(() => {
+    server = getRedisServer(new Store());
+    server.listen(6380, "127.0.0.1");
+  });
+
+  afterEach(() => {
+    server.close();
+  });
+
   describe("PING 명령을 받으면", () => {
-    let server;
-
-    beforeEach(() => {
-      server = getRedisServer(new Store());
-      server.listen(6380, "127.0.0.1");
-    });
-
-    afterEach(() => {
-      server.close();
-    });
-
     it("SimpleString 타입의 PONG 을 응답한다.", async () => {
       const response = await sendInput(
-        "*1\r\n$4\r\nPING\r\n",
-        server
+        "*1\r\n$4\r\nPING\r\n"
       );
 
       expect(response.toString()).toBe(
@@ -56,21 +52,9 @@ describe("Redis 서버 애플리케이션은", () => {
   });
 
   describe("ECHO 명령어를 받았을 때", () => {
-    let server;
-
-    beforeEach(() => {
-      server = getRedisServer(new Store());
-      server.listen(6380, "127.0.0.1");
-    });
-
-    afterEach(() => {
-      server.close();
-    });
-
     it("hey 라는 문자열을 인자로 받으면, SimpleString 타입의 hey 를 응답한다.", async () => {
       const data = await sendInput(
-        "*2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n",
-        server
+        "*2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n"
       );
 
       expect(data.toString()).toBe(
@@ -89,8 +73,7 @@ describe("Redis 서버 애플리케이션은", () => {
     testCases.forEach((testString) => {
       it(`${testString} 문자열을 인자로 받으면, SimpleString 타입의 ${testString} 를 응답한다.`, async () => {
         const data = await sendInput(
-          `*2\r\n$4\r\nECHO\r\n$${testString.length}\r\n${testString}\r\n`,
-          server
+          `*2\r\n$4\r\nECHO\r\n$${testString.length}\r\n${testString}\r\n`
         );
 
         expect(data.toString()).toBe(
@@ -101,21 +84,9 @@ describe("Redis 서버 애플리케이션은", () => {
   });
 
   describe("SET 명령어를 받았을 때", () => {
-    let server;
-
-    beforeEach(() => {
-      server = getRedisServer(new Store());
-      server.listen(6380, "127.0.0.1");
-    });
-
-    afterEach(() => {
-      server.close();
-    });
-
     it("유효한 key 와 value 를 받으면, SimpleString 타입의 OK 를 응답한다.", async () => {
       const data = await sendInput(
-        "*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n",
-        server
+        "*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n"
       );
 
       expect(data.toString()).toBe(
@@ -133,8 +104,7 @@ describe("Redis 서버 애플리케이션은", () => {
     testCases.forEach(([key, value]) => {
       it(`키로 ${key} 를 받고 값으로 ${value} 를 받으면, SimpleString 타입의 OK 를 응답한다.`, async () => {
         const data = await sendInput(
-          `*3\r\n$3\r\nSET\r\n$${key.length}\r\n${key}\r\n$${value.length}\r\n${value}\r\n`,
-          server
+          `*3\r\n$3\r\nSET\r\n$${key.length}\r\n${key}\r\n$${value.length}\r\n${value}\r\n`
         );
 
         expect(data.toString()).toBe(
@@ -145,21 +115,9 @@ describe("Redis 서버 애플리케이션은", () => {
   });
 
   describe("GET 명령어를 받았을 때", () => {
-    let server;
-
-    beforeEach(() => {
-      server = getRedisServer(new Store());
-      server.listen(6380, "127.0.0.1");
-    });
-
-    afterEach(() => {
-      server.close();
-    });
-
     it("존재하지 않는 키를 인자로 받으면, null bulk stirng 을 응답한다.", async () => {
       const data = await sendInput(
-        "*2\r\n$3\r\nGET\r\n$3\r\nkey\r\n",
-        server
+        "*2\r\n$3\r\nGET\r\n$3\r\nkey\r\n"
       );
 
       expect(data.toString()).toBe(
@@ -169,8 +127,7 @@ describe("Redis 서버 애플리케이션은", () => {
 
     it("존재하는 키를 인자로 받으면, 해당 키에 대한 값을 BulkString 타입으로 응답한다.", async () => {
       const data = await sendInput(
-        "*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n",
-        server
+        "*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n"
       );
 
       expect(data.toString()).toBe(
@@ -178,8 +135,7 @@ describe("Redis 서버 애플리케이션은", () => {
       );
 
       const data2 = await sendInput(
-        "*2\r\n$3\r\nGET\r\n$3\r\nkey\r\n",
-        server
+        "*2\r\n$3\r\nGET\r\n$3\r\nkey\r\n"
       );
 
       expect(data2.toString()).toBe(
@@ -189,8 +145,7 @@ describe("Redis 서버 애플리케이션은", () => {
 
     it("존재하는 키를 인자로 받으면, 만료되지 않았을 때 값을 BulkString 타입으로 응답한다.", async () => {
       const data = await sendInput(
-        "*5\r\n$3\r\nSET\r\n$6\r\nbanana\r\n$9\r\npineapple\r\n$2\r\npx\r\n$3\r\n100\r\n",
-        server
+        "*5\r\n$3\r\nSET\r\n$6\r\nbanana\r\n$9\r\npineapple\r\n$2\r\npx\r\n$3\r\n100\r\n"
       );
 
       expect(data.toString()).toBe(
@@ -198,8 +153,7 @@ describe("Redis 서버 애플리케이션은", () => {
       );
 
       const data2 = await sendInput(
-        "*2\r\n$3\r\nGET\r\n$6\r\nbanana\r\n",
-        server
+        "*2\r\n$3\r\nGET\r\n$6\r\nbanana\r\n"
       );
 
       expect(data2.toString()).toBe(
@@ -209,8 +163,7 @@ describe("Redis 서버 애플리케이션은", () => {
 
     it("존재하는 키를 인자로 받으면, 만료되었을 때 값을 NullBulkString 타입으로 응답한다.", async () => {
       const data = await sendInput(
-        "*5\r\n$3\r\nSET\r\n$6\r\nbanana\r\n$9\r\npineapple\r\n$2\r\npx\r\n:-100\r\n",
-        server
+        "*5\r\n$3\r\nSET\r\n$6\r\nbanana\r\n$9\r\npineapple\r\n$2\r\npx\r\n:-100\r\n"
       );
 
       expect(data.toString()).toBe(
@@ -218,8 +171,7 @@ describe("Redis 서버 애플리케이션은", () => {
       );
 
       const data2 = await sendInput(
-        "*2\r\n$3\r\nGET\r\n$6\r\nbanana\r\n",
-        server
+        "*2\r\n$3\r\nGET\r\n$6\r\nbanana\r\n"
       );
 
       expect(data2.toString()).toBe(
